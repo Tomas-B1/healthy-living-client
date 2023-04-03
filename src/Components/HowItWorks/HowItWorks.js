f0061b1d0df841960626def98b87d47f1464d9a4
Tomas <t.bojkovski@gmail.com>
2023-04-03 17:19:43 -0400

    adds cards for home page with rough styling

---
+++ src/Components/HowItWorks/HowItWorks.js
@@ -0,0 +1,34 @@
+import "./HowItWorks.scss";
+import checkmark from "../../assets/icons/check-symbol.png"
+import dumbbell from "../../assets/icons/dumbbell-symbol.png"
+import profile from "../../assets/icons/profile-symbol.png"
+// import { Link } from 'react-router-dom';
+
+function HomeHeader() {
+    return (
+        <section className="info">
+            <h2 className="info__title">How It Works</h2>
+            <div className="info__signup">
+                <img className="info__icon" src={checkmark} alt="checkmark"/>
+                <p className="info__text">Create your profile now to get started today!</p>
+                <button className="info__button">SIGN UP</button>
+            </div>
+            <div className="info__workouts">
+                <img className="info__icon" src={dumbbell} alt="dumbbell"/>
+                <p className="info__text">Find new workouts</p>
+            </div>
+            <div className="info__profile">
+                <img className="info__icon" src={profile} alt="profile"/>
+                <p className="info__text">Save workouts to your personal profile</p>
+            </div>
+            <div className="whyChoose">
+                <h2 className="whyChoose__title">WHY CHOOSE HEALTHY LIVING?</h2>
+                <div className="whyChoose__container">
+                    <p className="whyChoose__text">Joining the Healthy Living community will grant you access and the ability to save workouts to your profile, helping you obtain your fitness goals.</p>
+                </div>
+            </div>
+        </section>
+      );
+  }
+  
+  export default HomeHeader;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import HomeHeader from "../../Components/HomeHeader/HomeHeader"
+import HowItWorks from "../../Components/HowItWorks/HowItWorks"
 
 
 function Home() {
     return (
       <main className="home-page">
         <HomeHeader />
+        <HowItWorks />
       </main>
     );
   }
