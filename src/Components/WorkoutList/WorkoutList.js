6d5f672a96e05c5336559ad6162a8e2394e64161
Tomas <t.bojkovski@gmail.com>
2023-04-07 13:34:14 -0400

    adds functionality to dropdown menu to filter from muscle

--- src/Components/WorkoutList/WorkoutList.js
+++ src/Components/WorkoutList/WorkoutList.js
@@ -6,7 +6,7 @@ import "../WorkoutList/WorkoutList.scss"
 function WorkoutList() {
   const [workout, setWorkout] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [value, setValue]= useState("")
+  const [option, setOption]= useState("All Wokouts")
   let muscle;
 
   useEffect(() => {
@@ -17,45 +17,96 @@ function WorkoutList() {
     });
   }, []);
 
+  // let Biceps = workout.filter(function(work) {
+  //   return work.muscle === "Biceps";
+  //  });
+
+  //   workout.filter(function(work) {
+  //   return work.muscle === "Calves";
+  //   });
+
+  //   workout.filter(function(work) {
+  //   return work.muscle === "Pectorals";
+  //   });
+    
+  // let Traps =  workout.filter(function(work) {
+  //   return work.muscle === "Traps";
+  // });
+
+  // let Triceps =  workout.filter(function(work) {
+  //   return work.muscle === "Triceps";
+  // });
+
+  // let Quads =  workout.filter(function(work) {
+  //   return work.muscle === "Quads";
+  // });
+
+  // let Upper =  workout.filter(function(work) {
+  //   return work.muscle === "Upper Back";
+  // });
+
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
 
-  let Biceps =  workout.filter(function(work) {
-    return work.muscle === "Biceps";
-  });
-
-  console.log(Biceps);
-
-  let Calves =  workout.filter(function(work) {
-    return work.muscle === "Calves";
-  });
-
-  let Pectorals =  workout.filter(function(work) {
-    return work.muscle === "Pectorals";
-  });
-
-  let Traps =  workout.filter(function(work) {
-    return work.muscle === "Traps";
-  });
-
-  let Triceps =  workout.filter(function(work) {
-    return work.muscle === "Triceps";
-  });
-
-  let Quads =  workout.filter(function(work) {
-    return work.muscle === "Quads";
-  });
-
-  let Upper =  workout.filter(function(work) {
-    return work.muscle === "Upper Back";
-  });
-
   const handleChange =(event) => {
-     setValue(event.target.value)
+    if(event.target.value === "Biceps"){
+      muscle = workout.filter(
+        (work) => work.muscle === "Biceps"
+      );
+      setWorkout(muscle)
+      setOption(event.target.value)
+    }else if(event.target.value === "Calves"){
+      muscle = workout.filter(
+        (work) => work.muscle === "Calves"
+      );
+      setWorkout(muscle)
+      setOption(event.target.value)
+    }else if(event.target.value === "Pectorals"){
+      muscle = workout.filter(
+        (work) => work.muscle === "Pectorals"
+      );
+      setWorkout(muscle)
+      setOption(event.target.value)
+    } else if(event.target.value === "Traps"){
+      muscle = workout.filter(
+        (work) => work.muscle === "Traps"
+      );
+      setWorkout(muscle)
+      setOption(event.target.value)
+    } else if(event.target.value === "Triceps"){
+      muscle = workout.filter(
+        (work) => work.muscle === "Triceps"
+      );
+      setWorkout(muscle)
+      setOption(event.target.value)
+    } else if(event.target.value === "Quads"){
+      muscle = workout.filter(
+        (work) => work.muscle === "Quads"
+      );
+      setWorkout(muscle)
+      setOption(event.target.value)
+    } else if(event.target.value === "Upper Back"){
+        muscle = workout.filter(
+          (work) => work.muscle === "Upper back"
+        );
+        setWorkout(muscle)
+        setOption(event.target.value)
+    } else if(event.target.value === "Traps"){
+      muscle = workout.filter(
+        (work) => work.muscle === "Traps"
+       );
+      setWorkout(muscle)
+      setOption(event.target.value)
+   }  else if (event.target.value === "All Workouts"){
+      muscle = workout.filter(
+        (work) => work.muscle === "Biceps"||"Calves"||"Peactorals"||"Traps"||"Triceps"||"Quads"||"Upper back"
+      );
+      setWorkout(muscle)
+      setOption(event.target.value)
+  }
   }
 
-  
   return (
       <main className="workouts">
         <h1 className="workouts__title">Workouts</h1>
@@ -63,7 +114,7 @@ function WorkoutList() {
           className="workouts__dropdown"
           name="workouts"
           id="workouts"
-          value={value}
+          value={option}
           onChange={handleChange}
         >
           <option value="All">All Workouts</option>
